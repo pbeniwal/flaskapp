2b3a78870ed9a8d8c0f41a0e8f0e5c28b812abd9
Ubuntu <ubuntu@ip-172-31-83-140.ec2.internal>
2024-02-11 11:39:03 +0000

    updated app.py

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/hello')
 def hello_world():
-	return 'Hello, World!'
+	return 'Hello, World!\n'
+
+@app.route('/hello/<username>')
+def hello_user(username):
+	return 'Hello, %s!\n' % username
 
 if __name__ == "__main__":
     app.run()
